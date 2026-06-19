db879657395f70ebd8bc5d27947ba9b18c4c759f
kimsangjunv1 <ingyoe64@gmail.com>
2026-06-19 13:36:14 +0900

    fix: remove unused legacy files

--- src/shared/ui/common/TransitionOverlay.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { ReactNode } from "react";
-
-const TransitionOverlay = ({ children }: { children: ReactNode }) => {
-    return children;
-};
-
-export default TransitionOverlay;
